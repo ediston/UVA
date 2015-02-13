565a82ca09556766e4a32f6a5c8d4cf1b148ac90
ediston <vivek4dhiman@gmail.com>
2015-02-13 14:33:20 +0100

    Create 1124.cpp

---
+++ 1124.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "stdio.h"
+using namespace std;
+
+int main(){
+    char s[1000];
+    while(cin.getline (s,256)){
+            cout << s << endl;
+    }
+    return 0;
+}
